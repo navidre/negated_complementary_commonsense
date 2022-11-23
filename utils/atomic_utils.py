33af00a70481d1720dc0d74c283ea362d1938386
navidre <navidre@users.noreply.github.com>
2022-11-23 10:05:53 -0600

    code for per-predicate geenration is in progress

--- utils/atomic_utils.py
+++ utils/atomic_utils.py
@@ -1,3 +1,5 @@
 
 
-physical_preds = ['Objectuse', 'AtLocation', 'MadeUpOf', 'HasProperty', 'CapableOf', 'Desires', 'Not Desires']
+physical_preds = ['ObjectUse', 'AtLocation', 'MadeUpOf', 'HasProperty', 'CapableOf', 'Desires', 'Not Desires']
+event_preds = ['IsAfter', 'HasSubEvent', 'IsBefore', 'HinderedBy','Causes', 'xReason', 'isFilledBy']
+social_preds = ['xNeed', 'xAttr', 'xEffect', 'xReact', 'xWant', 'xIntent', 'oEffect', 'oReact', 'oWant']
