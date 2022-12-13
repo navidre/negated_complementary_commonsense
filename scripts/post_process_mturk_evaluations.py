3446520136ce871f2fa24d8a279f1588743ef2cc
navidre <navidre@users.noreply.github.com>
2022-12-13 14:51:29 -0600

    Added individual reviews to the output TSV file

--- scripts/post_process_mturk_evaluations.py
+++ scripts/post_process_mturk_evaluations.py
@@ -16,42 +16,53 @@ def update_out_tsv_from_manifest(mturk_path, out_tsv_path):
     # Extracting folder name
     folder_name = [ch for ch in mturk_path.split('/') if ch != ''][-1]
     metadata_key = f'{folder_name}-metadata'
-    # Loading output manifest
-    evaluations = {}
+    # Loading output manifest as an array
+    evaluations_list = []
     with open(f'{mturk_path}/manifests/output/output.manifest') as f:
-        # Loading output manifest as dict with source as key and answer content as value. Each key can have multiple values
         for line in f:
             line = json.loads(line)
-            source = line['source']
-            if source not in evaluations:
-                evaluations[source] = []
-            evaluations[source].append(line)
+            evaluations_list.append(line)
     # Loading output TSV file
     out_tsv_df = pd.read_csv(out_tsv_path, sep='\t', header=0)
+    # Annotations path
+    annotations_path = f'{mturk_path}/annotations/worker-response/iteration-1'
     # Iterate over the output manifest and update the output TSV file
     # Iterate over loaded TSV file
+    manifest_index = 0
     for index, row in tqdm(out_tsv_df.iterrows()):
         # skip if already auto-evaluated
-        if row['review_1'] != 0:
+        if row['review'] != 0:
             continue
         # Extracting the source
         source = row['full_text']
         # Extracting the review
-        consolidated_class_name = evaluations[source][0][metadata_key]['class-name']
+        consolidated_class_name = evaluations_list[manifest_index][metadata_key]['class-name']
         review = CLASS_TO_INDEX[consolidated_class_name]
         # Updating the output TSV file
         out_tsv_df.at[index, 'review'] = review
-        # Remove the evaluation from the dict
-        evaluations[source].pop(0)
         # TODO: Add individual reviews
-        # # Load the individual reviews
+        # Load the individual reviews
         # worker_responses = evaluations[source][0][metadata_key]['worker-responses']
+        annotation_json_folder_path = f'{annotations_path}/{manifest_index}'
+        annotation_json_files = [f for f in os.listdir(annotation_json_folder_path) if os.path.isfile(os.path.join(annotation_json_folder_path, f))]
+        # Iterate over the individual reviews
+        assert len(annotation_json_files) == 1, f'Found more than one or no annotation files for {source}; Update implemetation to handle this case!'
+        annotation_file_path = f'{annotation_json_folder_path}/{annotation_json_files[0]}'
+        with open(annotation_file_path) as f:
+            annotation_json = json.load(f)
+            answer_jsons = annotation_json['answers']
+            i = 1 # review index starts from 1
+            for answer_json in answer_jsons:
+                # Extracting the review
+                class_name = answer_json['answerContent']['crowd-classifier']['label']
+                review = CLASS_TO_INDEX[class_name]
+                # Updating the output TSV file
+                out_tsv_df.at[index, f'review_{i}'] = review
+                i += 1
+        # Update the manifest index (note that auto-evaluated rows are skipped in the output TSV file only)
+        manifest_index += 1
 
-    # Sanity check and assert that all evaluations are used
-    for k, v in evaluations.items():
-        assert len(v) == 0, f'Not all evaluations are used; Index: {k}'
     # TODO: Calculate alpha score (agreement between reviewers) and store in a sepapte txt file
-    import IPython; IPython. embed(); exit(1)
     # Save the updated output TSV file
     out_tsv_df.to_csv(out_tsv_path, sep='\t', index=False)
 
