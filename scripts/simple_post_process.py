0e344068ecbaef1a3d71009d5fafd755f46d07e3
navidre <navidre@users.noreply.github.com>
2022-12-27 18:47:38 -0600

    fixes in code and negated-few-shot-limited

--- scripts/simple_post_process.py
+++ scripts/simple_post_process.py
@@ -1,28 +1,34 @@
 import argparse, os
+from pathlib import Path
 
 if __name__ == "__main__":
 
     """Usage example:
-    python scripts/simple_post_process.py --s3_path s3://negated-predicates/sampled_10_atomic2020_cot_qa_limited_preds/negated/ --experiment_folder ./experiments/sampled_10_atomic2020_cot_qa_limited_preds --out_tsv experiments/sampled_10_atomic2020_cot_qa_limited_preds/sampled_negated_preds_generated_cot_qa_evaluated.tsv
+    python scripts/simple_post_process.py --s3_path s3://negated-predicates/sampled_10_atomic2020_few_shot_limited_preds/negated-few-shot-limited/ --evaluated_jsonl_filename sampled_negated_preds_generated_few_shot_mturk.jsonl
     """
 
     parser = argparse.ArgumentParser()
     # S3 path to the results. Can be copied from the Labeling job's console, under "Output dataset location".
-    parser.add_argument("--s3_path", type=str, default=f's3://negated-predicates/self_samples/sample-negated/results/samples-negated/')
-    # experiment_folder is the experiment folder, including the TSV generation file, which was converted to JSONL and uploaded to S3 for mTurk evaluation.
-    parser.add_argument("--experiment_folder", type=str, default=f'./experiments/test_experiment')
+    parser.add_argument("--s3_path", type=str, default=f's3://negated-predicates/sampled_10_atomic2020_few_shot_limited_preds/negated-few-shot-limited/')
     # The TSV generation file, which was converted to JSONL and uploaded to S3 for mTurk evaluation.
     # The name can be see under Labeling job's console, under "Input dataset location".
     # Use the relative local path.
-    parser.add_argument("--out_tsv", type=str, default=f'./experiments/test_experiment/sample.tsv')
+    parser.add_argument("--evaluated_jsonl_filename", type=str, default=f'sampled_negated_preds_generated_few_shot_mturk.jsonl')
     args = parser.parse_args()
 
+    # Extracting experiment name from s3_path and the constructing experiment folder
+    experiment_name = str(Path(args.s3_path).parent).split('/')[-1]
+    experiment_folder = f'experiments/{experiment_name}'
+
+    # Constructing out_tsv file name and path
+    out_tsv_filename = os.path.basename(args.evaluated_jsonl_filename).replace('_mturk.jsonl', '_evaluated.tsv')
+    out_tsv_path = f'{experiment_folder}/{out_tsv_filename}'
 
     # Download the results
     print('*** Downloading the results ***')
     experiment_name = args.s3_path.split('/')[-2]
     # Create mturk folder if not exists
-    mturk_parent_path = f'{args.experiment_folder}/mturk'
+    mturk_parent_path = f'{experiment_folder}/mturk'
     if not os.path.exists(mturk_parent_path):
         os.makedirs(mturk_parent_path)
     mturk_path = f'{mturk_parent_path}/{experiment_name}'
@@ -32,7 +38,7 @@ if __name__ == "__main__":
 
     # Post-process the results
     print('*** Post-processing the results ***')
-    os.system(f'python scripts/post_process_mturk_evaluations.py --mturk_path {mturk_path} --out_tsv {args.out_tsv}')
+    os.system(f'python scripts/post_process_mturk_evaluations.py --mturk_path {mturk_path} --out_tsv {out_tsv_path}')
     # Plot the results
     print('*** Plotting the results ***')
-    os.system(f'python scripts/plot_evaluated_results.py --in_tsv {args.out_tsv}')
+    os.system(f'python scripts/plot_evaluated_results.py --in_tsv {out_tsv_path}')
