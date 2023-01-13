4736375c165c92990a4b3d9d7d3a1729e4bf7d87
navidre <navidre@users.noreply.github.com>
2023-01-13 15:50:36 -0600

    majority votes added

--- scripts/compare_methods.py
+++ scripts/compare_methods.py
@@ -42,11 +42,18 @@ if __name__ == "__main__":
         if not os.path.exists(experiment_path):
             os.makedirs(experiment_path)
         # Constructing out_tsv file name and path
+        # SageMaker review
         out_tsv_filename = f'compared_results.tsv'
         out_tsv_path = f'{experiment_path}/{out_tsv_filename}'
+        # Majority vote
+        out_tsv_filename_majority = f'compared_results_majority.tsv'
+        out_tsv_path_majority = f'{experiment_path}/{out_tsv_filename_majority}'
         # Create the out_tsv file if not exists
         if not os.path.exists(out_tsv_path):
             with open(out_tsv_path, 'w') as f:
+                f.write('method\tnormal_accuracy\tnegated_accuracy\n')
+        if not os.path.exists(out_tsv_path_majority):
+            with open(out_tsv_path_majority, 'w') as f:
                 f.write('method\tnormal_accuracy\tnegated_accuracy\n')
         
         # Post-process each method
@@ -59,6 +66,7 @@ if __name__ == "__main__":
             normal_evaluation_filename = f'sampled_normal_preds_generated_{method}_evaluated.tsv'
             os.system(f'python scripts/simple_post_process.py --in_tsv {folder_path}/{normal_evaluation_filename}')
             # Get accuracy of the method from the evaluation JSON file sampled_negated_preds_generated_cot_qa_evaluated_results.json
+            
             # Open the evaluation JSON file
             # Negated
             negated_json_evaluation_filename = f'sampled_negated_preds_generated_{method}_evaluated_results.json'
@@ -71,7 +79,22 @@ if __name__ == "__main__":
             with open(f'{folder_path}/{normal_json_evaluation_filename}', 'r') as f:
                 normal_results = json.load(f)
                 normal_accuracy = int(normal_results['total']['correct'])*100.0/int(normal_results['total']['count'])
-
             # Append the results to the out_tsv file
             with open(out_tsv_path, 'a') as f:
                 f.write(f'{method}\t{normal_accuracy}\t{negated_accuracy}\n')
+            
+            # Majority vote
+            # Negated
+            negated_json_evaluation_filename_majority = f'sampled_negated_preds_generated_{method}_evaluated_results_majority.json'
+            majority_negated_accuracy, majority_normal_accuracy = 0, 0
+            with open(f'{folder_path}/{negated_json_evaluation_filename_majority}', 'r') as f:
+                negated_results = json.load(f)
+                majority_negated_accuracy = int(negated_results['total']['correct'])*100.0/int(negated_results['total']['count'])
+            # Normal
+            normal_json_evaluation_filename_majority = f'sampled_normal_preds_generated_{method}_evaluated_results_majority.json'
+            with open(f'{folder_path}/{normal_json_evaluation_filename_majority}', 'r') as f:
+                normal_results = json.load(f)
+                majority_normal_accuracy = int(normal_results['total']['correct'])*100.0/int(normal_results['total']['count'])
+            # Append the results to the out_tsv file
+            with open(out_tsv_path_majority, 'a') as f:
+                f.write(f'{method}\t{majority_normal_accuracy}\t{majority_negated_accuracy}\n')
--- scripts/plot_evaluated_results.py
+++ scripts/plot_evaluated_results.py
@@ -10,8 +10,10 @@ def count_annotations(in_tsv):
     # Extracting file name
     filename = os.path.basename(in_tsv).split('.')[0]
     out_filename = f'{filename}_results'
+    out_filename_majority = f'{filename}_results_majority'
     work_path = os.path.dirname(args.in_tsv)
     out_json = os.path.join(work_path, f'{out_filename}.json')
+    out_json_majority = os.path.join(work_path, f'{out_filename_majority}.json')
     # read tsv file with columns head, relation, prompt, generated_tail, full_text
     df = read_csv(in_tsv, sep='\t', header=0)
     # Calculate review accuracy by relation. Have count and unique values
@@ -19,71 +21,105 @@ def count_annotations(in_tsv):
     # Group df by relation
     df_review = df.groupby(['relation'])
     results = {}
+    majority_results = {}
     total_count, total_correct, total_incorrect, total_unfamiliar = 0, 0, 0, 0
+    total_count_majority, total_correct_majority, total_incorrect_majority, total_unfamiliar_majority = 0, 0, 0, 0
     for name, group in df_review:
         total, count_correct, count_incorrect, count_unfamiliar = group['review'].count(), 0, 0, 0
+        if 'majority_vote' not in group.columns:
+            group['majority_vote'] = 0
+        total_majority, count_correct_majority, count_incorrect_majority, count_unfamiliar_majority = group['majority_vote'].count(), 0, 0, 0
         # Count unfamiliar as review 5
         for index, row in group.iterrows():
+            # Count review
             if row['review'] == 1 or row['review'] == 2:
                 count_correct += 1
             elif row['review'] == 3 or row['review'] == 4:
                 count_incorrect += 1
             elif row['review'] == 5:
                 count_unfamiliar += 1
-        # Count totals
+            # Count majority vote
+            if row['majority_vote'] == 1 or row['majority_vote'] == 2:
+                count_correct_majority += 1
+            elif row['majority_vote'] == 3 or row['majority_vote'] == 4:
+                count_incorrect_majority += 1
+            elif row['majority_vote'] == 5:
+                count_unfamiliar_majority += 1
+        # Count review totals
         total_count += total
         total_correct += count_correct
         total_incorrect += count_incorrect
         total_unfamiliar += count_unfamiliar
+        # Count majority vote totals
+        total_count_majority += total_majority
+        total_correct_majority += count_correct_majority
+        total_incorrect_majority += count_incorrect_majority
+        total_unfamiliar_majority += count_unfamiliar_majority
         # Store results
         results[name] = {'count': total,
                          'correct': count_correct,
                          'incorrect': count_incorrect,
                          'unfamiliar': count_unfamiliar
                         }
+        majority_results[name] = {'count': total_majority,
+                                  'correct': count_correct_majority,
+                                  'incorrect': count_incorrect_majority,
+                                  'unfamiliar': count_unfamiliar_majority
+                                 }
     # Add totals
     results['total'] = {'count': total_count,
                         'correct': total_correct,
                         'incorrect': total_incorrect,
                         'unfamiliar': total_unfamiliar
                         }
+    majority_results['total'] = {'count': total_count_majority,
+                                 'correct': total_correct_majority,
+                                 'incorrect': total_incorrect_majority,
+                                 'unfamiliar': total_unfamiliar_majority
+                                 }
     # Save results as JSON file in work_path directory 
     with open(out_json, 'w') as f:
         json.dump(results, f, indent=4, default=str)
+    with open(out_json_majority, 'w') as f:
+        json.dump(majority_results, f, indent=4, default=str)
     # Return results
-    return results
+    return results, majority_results
 
-def plot_the_results(results, in_tsv):
+def plot_the_results(results,  majority_results, in_tsv):
     print(f"Plotting results from {in_tsv}")
     # Extracting file name
     filename = os.path.basename(in_tsv).split('.')[0]
     out_filename = f'{filename}_results'
+    out_filename_majority = f'{filename}_results_majority'
     work_path = os.path.dirname(args.in_tsv)
-    out_pdf = os.path.join(work_path, f'{out_filename}.pdf')
     # DataFrame from results dictionary
-    df = pd.DataFrame.from_dict(results, orient='index')
-    # Divide each row by count column to get percentages
-    df_norm = df.div(df['count'], axis=0)
-    # Set index as column 'relation'
-    df_norm = df_norm.reset_index()
-    # Sort by incorrect column
-    df_norm = df_norm.sort_values(by=['incorrect'], ascending=False)
-    # Plot
-    df_norm.plot(x='index', y=['correct', 'incorrect', 'unfamiliar'], kind='bar')
-    # Set title
-    plt.title('Review accuracy by relation')
-    # Set x label
-    plt.xlabel('Relation')
-    # Set y label
-    plt.ylabel('Percentage')
-    # Make plot wider
-    plt.gcf().set_size_inches(20, 10)
-    # Rotate x labels by 45 degrees
-    plt.xticks(rotation=45, ha = 'right')
-    # Increase font size
-    plt.rcParams.update({'font.size': 22})
-    # Save plot as pdf
-    plt.savefig(out_pdf, bbox_inches='tight')
+    dfs = [pd.DataFrame.from_dict(results, orient='index'), pd.DataFrame.from_dict(majority_results, orient='index')]
+    out_pdfs = [os.path.join(work_path, f'{out_filename}.pdf'), os.path.join(work_path, f'{out_filename_majority}.pdf')]
+    index = 0
+    for df in dfs:
+        # Divide each row by count column to get percentages
+        df_norm = df.div(df['count'], axis=0)
+        # Set index as column 'relation'
+        df_norm = df_norm.reset_index()
+        # Sort by incorrect column
+        df_norm = df_norm.sort_values(by=['incorrect'], ascending=False)
+        # Plot
+        df_norm.plot(x='index', y=['correct', 'incorrect', 'unfamiliar'], kind='bar')
+        # Set title
+        plt.title('Review accuracy by relation')
+        # Set x label
+        plt.xlabel('Relation')
+        # Set y label
+        plt.ylabel('Percentage')
+        # Make plot wider
+        plt.gcf().set_size_inches(20, 10)
+        # Rotate x labels by 45 degrees
+        plt.xticks(rotation=45, ha = 'right')
+        # Increase font size
+        plt.rcParams.update({'font.size': 22})
+        # Save plot as pdf
+        plt.savefig(out_pdfs[index], bbox_inches='tight')
+        index += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -96,5 +132,5 @@ if __name__ == "__main__":
     python scripts/plot_evaluated_results.py --in_tsv experiments/atomic_2020_eval/few_shot_sampled_to_eval_with_gpt_3_self_evaluated.tsv
     """
 
-    results = count_annotations(args.in_tsv)
-    plot_the_results(results, args.in_tsv)
+    results, majority_results = count_annotations(args.in_tsv)
+    plot_the_results(results, majority_results, args.in_tsv)
