cfa7e1c60a7ed27041534f0a77c3b101f2f3c5cc
navidre <navidre@users.noreply.github.com>
2023-01-19 11:16:52 -0600

    fixed issues in compare_method and updated to support the new subfolder format

--- scripts/compare_methods.py
+++ scripts/compare_methods.py
@@ -22,6 +22,15 @@ EXPERIMENTS = {
     }
 }
 
+def extract_accurcy_from_json(json_filepath):
+    accuracy = 0
+    # check if file exists and extract the accuracy
+    if os.path.exists(json_filepath):
+        with open(json_filepath, 'r') as f:
+            results = json.load(f)
+            accuracy = int(results['total']['correct'])*100.0/int(results['total']['count'])
+    return accuracy
+
 if __name__ == "__main__":
 
     """Usage example:
@@ -32,7 +41,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for experiment, folder_paths in tqdm(EXPERIMENTS.items(), desc='Experiments'):
-        # Sanity check
+        # Sanity check for the experiments existence
         if '' in folder_paths.values():
             print(f'*** Skipping {experiment} ***')
             print(f'*** Not all experiments are finished! ***')
@@ -51,55 +60,57 @@ if __name__ == "__main__":
         # Create the out_tsv file if not exists
         if not os.path.exists(out_tsv_path):
             with open(out_tsv_path, 'w') as f:
-                f.write('method\tnormal_accuracy\tnegated_accuracy\n')
+                f.write('method\tsub_folder\tnormal_accuracy\tnegated_accuracy\n')
         if not os.path.exists(out_tsv_path_majority):
             with open(out_tsv_path_majority, 'w') as f:
-                f.write('method\tnormal_accuracy\tnegated_accuracy\n')
+                f.write('method\tsubfolder\tnormal_accuracy\tnegated_accuracy\n')
         
         # Post-process each method
         for method, folder_path in folder_paths.items():
 
-            #region Loading the evaluations
-            print(f'*** Evaluating {method} in {experiment} ***')
-            # Negated
-            negated_evaluation_filename = f'sampled_negated_preds_generated_{method}_evaluated.tsv'
-            os.system(f'python scripts/simple_post_process.py --in_tsv {folder_path}/{negated_evaluation_filename}')    
-            # Normal
-            normal_evaluation_filename = f'sampled_normal_preds_generated_{method}_evaluated.tsv'
-            os.system(f'python scripts/simple_post_process.py --in_tsv {folder_path}/{normal_evaluation_filename}')
-            #endregion
+            # Each method result has multiple subfolders resulting from different human evaluation rounds
+            # We need to post-process each subfolder
+            subfolders = [f.path for f in os.scandir(folder_path) if f.is_dir() and '/mturk' not in f.path]
+            for subfolder in subfolders:
+                subfolder_name = subfolder.split('/')[-1]
+                #region Loading the evaluations
+                print(f'*** Evaluating {method} in {experiment} ***')
+                # Negated
+                # Check if subfolder is related to negated questions
+                if '-negated-' in subfolder_name:
+                    negated_evaluation_filename = f'sampled_negated_preds_generated_{method}_evaluated.tsv'
+                    os.system(f'python scripts/simple_post_process.py --in_tsv {subfolder}/{negated_evaluation_filename}')    
+                # Normal
+                # Check if subfolder is related to normal questions
+                if '-normal-' in subfolder_name:
+                    normal_evaluation_filename = f'sampled_normal_preds_generated_{method}_evaluated.tsv'
+                    os.system(f'python scripts/simple_post_process.py --in_tsv {subfolder}/{normal_evaluation_filename}')
+                #endregion
 
-            #region Calculating the accuracy based on SageMaker review
-            # Open the evaluation JSON file
-            # Negated
-            negated_json_evaluation_filename = f'sampled_negated_preds_generated_{method}_evaluated_results.json'
-            negated_accuracy, normal_accuracy = 0, 0
-            with open(f'{folder_path}/{negated_json_evaluation_filename}', 'r') as f:
-                negated_results = json.load(f)
-                negated_accuracy = int(negated_results['total']['correct'])*100.0/int(negated_results['total']['count'])
-            # Normal
-            normal_json_evaluation_filename = f'sampled_normal_preds_generated_{method}_evaluated_results.json'
-            with open(f'{folder_path}/{normal_json_evaluation_filename}', 'r') as f:
-                normal_results = json.load(f)
-                normal_accuracy = int(normal_results['total']['correct'])*100.0/int(normal_results['total']['count'])
-            # Append the results to the out_tsv file
-            with open(out_tsv_path, 'a') as f:
-                f.write(f'{method}\t{normal_accuracy}\t{negated_accuracy}\n')
-            #endregion
+                #region Calculating the accuracy based on SageMaker review
+                # Negated
+                negated_json_evaluation_filename = f'sampled_negated_preds_generated_{method}_evaluated_results.json'
+                negated_filepath = f'{subfolder}/{negated_json_evaluation_filename}'
+                negated_accuracy = extract_accurcy_from_json(negated_filepath)
+                # Normal
+                normal_json_evaluation_filename = f'sampled_normal_preds_generated_{method}_evaluated_results.json'
+                normal_filepath = f'{subfolder}/{normal_json_evaluation_filename}'
+                normal_accuracy = extract_accurcy_from_json(normal_filepath)
+                # Append the results to the out_tsv file
+                with open(out_tsv_path, 'a') as f:
+                    f.write(f'{method}\t{subfolder_name}\t{normal_accuracy}\t{negated_accuracy}\n')
+                #endregion
             
-            #region Calculating the accuracy based on majority vote
-            # Negated
-            negated_json_evaluation_filename_majority = f'sampled_negated_preds_generated_{method}_evaluated_results_majority.json'
-            majority_negated_accuracy, majority_normal_accuracy = 0, 0
-            with open(f'{folder_path}/{negated_json_evaluation_filename_majority}', 'r') as f:
-                negated_results = json.load(f)
-                majority_negated_accuracy = int(negated_results['total']['correct'])*100.0/int(negated_results['total']['count'])
-            # Normal
-            normal_json_evaluation_filename_majority = f'sampled_normal_preds_generated_{method}_evaluated_results_majority.json'
-            with open(f'{folder_path}/{normal_json_evaluation_filename_majority}', 'r') as f:
-                normal_results = json.load(f)
-                majority_normal_accuracy = int(normal_results['total']['correct'])*100.0/int(normal_results['total']['count'])
-            # Append the results to the out_tsv file
-            with open(out_tsv_path_majority, 'a') as f:
-                f.write(f'{method}\t{majority_normal_accuracy}\t{majority_negated_accuracy}\n')
-            #endregion
+                #region Calculating the accuracy based on majority vote
+                # Negated
+                negated_json_evaluation_filename_majority = f'sampled_negated_preds_generated_{method}_evaluated_results_majority.json'
+                negated_majority_filepath = f'{subfolder}/{negated_json_evaluation_filename_majority}'
+                negated_majority_accuracy = extract_accurcy_from_json(negated_majority_filepath)
+                # Normal
+                normal_json_evaluation_filename_majority = f'sampled_normal_preds_generated_{method}_evaluated_results_majority.json'
+                normal_majority_filepath = f'{subfolder}/{normal_json_evaluation_filename_majority}'
+                normal_majority_accuracy = extract_accurcy_from_json(normal_majority_filepath)
+                # Append the results to the out_tsv file
+                with open(out_tsv_path_majority, 'a') as f:
+                    f.write(f'{method}\t{subfolder_name}\t{normal_majority_accuracy}\t{negated_majority_accuracy}\n')
+                #endregion
